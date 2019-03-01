18b432018f3dec83b89e35748ba070690ef3c844
Robin Han <33820379+robinhanstuy@users.noreply.github.com>
2019-03-01 10:51:30 -0500

    Update whoop.py

--- 06_mongo/whoop.py
+++ 06_mongo/whoop.py
@@ -6,11 +6,25 @@ db=connection.test
 collection=db.restaurants
 
 #bourough
-x = "Queens"
 def findBorough(borough):
     found = collection.find({"borough":borough})
     for item in found:
         print("Displaying restaurants in borough: " + borough + "\n")
         print("Name: "+ item['name'] + "\n")
 
-findBorough(x)
+def findZipcode(zipcode):
+    found = collection.find({"address.zipcode":zipcode})
+    for item in found:
+        print("Displaying restaurants in zipcode: " + zipcode + "\n")
+        print("ZipCode: "+ item['name']+ "\n")
+        
+def findZipGrade(zipcode,grade):
+    found = collection.find({"address.zipcode":zipcode,"grades.grade":grade})
+    for item in found:
+        print("Displaying restaurants in zipcode: " + zipcode + " and grade: " + grade + "\n")
+        print("Zip + Grade: "+ item['name']+ "\n")
+def findZipScore(zipcode,score):
+    found = collection.find({"address.zipcode":zipcode,"grades.score":{"$lt": score}})
+    for item in found:
+        print("displaying with zip: " + zipcode + " and score below :" + score + "\n")
+        print("Zip+Score: " + item['name']+ "\n")
