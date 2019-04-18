6c15e0363dd0c6c321bb3f071a0bad539b00984a
robinhanstuy <rhan@stuy.edu>
2019-04-18 08:29:51 -0400

    .

--- 20_anon-reduce/app.py
+++ 20_anon-reduce/app.py
@@ -1,4 +1,33 @@
+# Robin Han
+# SoftDev2 pd7
+# K20 -- Reductio ad Absurdum
+# 2019-04-18
+
 from functools import reduce
 
-def getFrequency(word):
-    
+f = open('book.txt', 'r')
+text = f.read().replace('.', ' ').replace(',', ' ').replace('?', ' ').replace('!', ' ')
+words = text.split()
+
+def getFreq(word):
+    lst = [0] + words
+    return reduce((lambda x, y: x + 1 if y == word else x), lst)
+
+def totalFreq(txt):
+    freq = [getFreq(x) for x in txt]
+    return reduce((lambda x, y: x + y), freq)
+
+def count(tmp, word):
+    if word in tmp:
+        tmp[word] += 1
+    else:
+        tmp[word] = 1
+    return tmp
+
+def mostFreq():
+    lst = [{}] + words
+    tmp = reduce(count, lst)
+    return max(tmp, key=tmp.get)
+
+print(getFreq('that'))
+print(mostFreq())
