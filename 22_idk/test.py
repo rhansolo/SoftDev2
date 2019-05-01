20d17018efac8a282ac1ccf98de3f2381161fe38
robinhanstuy <rhan@stuy.edu>
2019-04-30 22:51:11 -0400

    .

--- 22_idk/test.py
+++ 22_idk/test.py
@@ -1,3 +1,40 @@
+'''
+Robin Han
+SoftDev pd07
+K #22: Closure
+2019-05-01
+'''
+def make_counter():
+    count = 0
+    def inc():
+        nonlocal count
+        count += 1
+        return count
+    def get_count():
+        return counter
+    def option(print=False):
+        if print:
+            return get_count()
+        else:
+            return inc()
+    return option
+
+ctr1 = make_counter()
+print(ctr1())
+print(ctr1())
+print("count is ")
+print(ctr1(True))
+ctr2 = make_counter()
+print(ctr2())
+print(ctr1())
+print(ctr2())
+
+
+
+
+
+
+'''
 def inc(x):
     return x+1
 f = inc
@@ -14,20 +51,20 @@ def caller(c):
     print(c(3,5))
 
 caller(adder)
-"""
+
 def outer(x):
     def contains(l):
         return x in l
     return contains
-"""
+
 #contains_15 = outer(15)
-'''
+
 print(contains_15([1,2,3,4,5]))
 print(contains_15([13,14,15,16,17]))
 print(contains_15(range(1,20)))
 #del outer
 contains_15(range(14,20))
-'''
+
 def outer():
     x = "foo"
     def inner():
@@ -36,14 +73,4 @@ def outer():
     inner()
     return x
 print(outer())
-
-
-def makecounter():
-    count = 0
-    def inner():
-        nonlocal count += 1
-        return count
-    return inner
-ctrl = makecounter()
-print(ctrl())
-print(ctrl())
+'''
