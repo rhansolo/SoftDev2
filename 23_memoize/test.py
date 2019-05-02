5074d20b7c16f823a76b1803ae04eb4b72339b08
robinhanstuy <rhan@stuy.edu>
2019-05-02 13:05:49 -0400

    .

--- 23_memoize/test.py
+++ 23_memoize/test.py
@@ -19,18 +19,21 @@ print(greet())
 '''
 
 def memoize(f):
-    memo={}
+    memo = {}
     def helper(x):
-        
+        if x not in memo:
+            memo[x] = f(x)
+        return memo[x]
     return helper
 
+@memoize
 def fib(n):
     if n == 0:
         return 0
     elif n == 1:
-        return 1:
+        return 1
     else:
         return fib(n-1) + fib(n-2)
 
-fib = memoize(fib)
-print(fib(40))
+for x in range(0,50):
+    print(fib(x))
